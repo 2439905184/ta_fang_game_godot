b9be77875ac79c188aa2bff22b894e6ab5c8708b
2439905184 <29478722+2439905184@users.noreply.github.com>
2019-07-20 15:48:27 +0800

    Add files via upload

--- ta_fang/script/first_bo.gd
+++ ta_fang/script/first_bo.gd
@@ -5,20 +5,22 @@ extends Timer
 # var b = "text"
 var root
 var born_pos
+var path_root
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	root=find_parent("Node2D")
 	born_pos=root.get_node("enemy_pos").position
+	path_root=root.get_node("Path2D/PathFollow2D")
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
 
 
 func _on_Timer_timeout():
 	print_debug("第一波即将来了")
 	var enemy=load("res://enemy.tscn").instance()
-	root.add_child(enemy)
-	enemy.position=born_pos
+	path_root.add_child(enemy)
+	
 	pass # Replace with function body.
